7c4a2d9ce420763884acd53d3719c1689783b56f
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-02 17:30:20 +0600

    Add files via upload

---
+++ Vowel or Consonant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    int LowercaseVowel,UppercaseVowel;
+    printf("Enter An alphabet:");
+    scanf("%c",&c);
+    LowercaseVowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    UppercaseVowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+    if(LowercaseVowel || UppercaseVowel)
+        printf("%c is a vowel.",c);
+    else
+        printf("%c is a consonant.",c);
+
+}
